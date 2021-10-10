5d4b083f6ea7849090062922e6c649470e5262c0
hKouja <hassanalhomsi42@gmail.com>
2021-10-10 19:59:04 +0200

    test

--- src/main/scala/BlockBattle/Game.scala
+++ src/main/scala/BlockBattle/Game.scala
@@ -1,7 +1,5 @@
 package BlockBattle
 
-import BlockBattle.Game.Color.backgroundColorAtDepth
-import BlockBattle.Game.{windowSize, windowTitle}
 
 object Game {
   val windowSize = (30, 50)
@@ -13,14 +11,14 @@ object Game {
   object Color {
     import java.awt.Color as JColor
 
-    val black = new JColor(0, 0, 0)
-    val mole = new JColor(51,51, 0)
-    val mole2 = new JColor(60,60,0)
-    val soil =  new JColor(153, 102, 51)
+    val black  = new JColor(0, 0, 0)
+    val mole   = new JColor(51,51, 0)
+    val mole2  = new JColor(60,60,0)
+    val soil   = new JColor(153, 102, 51)
     val tunnel = new JColor(204, 153, 102)
-    val grass = new JColor(25, 130, 35)
-    val sky = new JColor(31, 190, 214)
-    val worm = new JColor(225, 100, 235)
+    val grass  = new JColor(25, 130, 35)
+    val sky    = new JColor(31, 190, 214)
+    val worm   = new JColor(225, 100, 235)
 
     def backgroundColorAtDepth (y: Int): java.awt.Color = {
       if (Game.skyRange contains(y)) {Color.sky}
@@ -38,14 +36,14 @@ class Game (
   import Game._
 
   val window = new BlockWindow(windowSize, windowTitle, blockSize)
-  val leftMole = new Mole (
+  val leftMole = new Mole(
     name = leftPlayerName,
     Pos (1,2),
     (1,0),
     Color.mole,
     KeyControl("a", "d", "w", "s")
   )
-  val rightMole = new Mole (
+  val rightMole = new Mole(
     name = rightPlayerName,
     Pos (1,2),
     (0,1),
